6a0b93637b0eb0021e9db188416d46ea7a03679d
return996 <2208604865@qq.com>
2020-05-20 21:08:41 +0800

    add eureka module

--- springcloud-consumer-dept-80/src/main/java/com/wang/springcloud/DeptConsumer_80.java
+++ springcloud-consumer-dept-80/src/main/java/com/wang/springcloud/DeptConsumer_80.java
@@ -5,6 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动程序
+ *
+ * @author Tim
+ */
+
 @SpringBootApplication
 public class DeptConsumer_80 {
 
--- springcloud-eureka-7001/src/main/java/com/wang/springcloud/EurekaServer_7001.java
+++ springcloud-eureka-7001/src/main/java/com/wang/springcloud/EurekaServer_7001.java
@@ -4,20 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
-/**
- * 启动类
- * @author Tim
- */
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-@EnableDiscoveryClient
-public class DeptProvider_8001 {
+@EnableEurekaServer
+public class EurekaServer_7001 {
 
-    private static final Logger log = LoggerFactory.getLogger( DeptProvider_8001.class );
+    private static final Logger log = LoggerFactory.getLogger( EurekaServer_7001.class );
 
     public static void main(String[] args) {
 
@@ -33,7 +26,7 @@ public class DeptProvider_8001 {
         log.warn("==================== WARN提示信息 ====================");
         log.warn("==================== WARN提示信息 ====================");
 
-        SpringApplication.run( DeptProvider_8001.class, args );
+        SpringApplication.run( EurekaServer_7001.class ,args );
 
     }
 
