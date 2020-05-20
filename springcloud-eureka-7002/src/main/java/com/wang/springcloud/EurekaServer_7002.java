931f659f2515ee9183635db42784ec4efd4111e2
return996 <2208604865@qq.com>
2020-05-21 07:22:16 +0800

    commit

---
+++ springcloud-eureka-7002/src/main/java/com/wang/springcloud/EurekaServer_7002.java
@@ -0,0 +1,32 @@
+package com.wang.springcloud;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer_7002 {
+
+    private static final Logger log = LoggerFactory.getLogger( EurekaServer_7002.class );
+
+    public static void main(String[] args) {
+
+        log.info("==================== INFO提示信息 ====================");
+        log.info("==================== INFO提示信息 ====================");
+        log.info("==================== INFO提示信息 ====================");
+        log.info("==================== INFO提示信息 ====================");
+        log.info("==================== INFO提示信息 ====================");
+
+        log.warn("==================== WARN提示信息 ====================");
+        log.warn("==================== WARN提示信息 ====================");
+        log.warn("==================== WARN提示信息 ====================");
+        log.warn("==================== WARN提示信息 ====================");
+        log.warn("==================== WARN提示信息 ====================");
+
+        SpringApplication.run(EurekaServer_7002.class, args);
+    }
+
+}
---
+++ springcloud-eureka-7003/src/main/java/com/wang/springcloud/EurekaServer_7003.java
@@ -0,0 +1,35 @@
+package com.wang.springcloud;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Chenxi Wang
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer_7003 {
+
+    private static final Logger log = LoggerFactory.getLogger( EurekaServer_7003.class );
+
+    public static void main(String[] args) {
+
+        log.info("==================== INFO提示信息 ====================");
+        log.info("==================== INFO提示信息 ====================");
+        log.info("==================== INFO提示信息 ====================");
+        log.info("==================== INFO提示信息 ====================");
+        log.info("==================== INFO提示信息 ====================");
+
+        log.warn("==================== WARN提示信息 ====================");
+        log.warn("==================== WARN提示信息 ====================");
+        log.warn("==================== WARN提示信息 ====================");
+        log.warn("==================== WARN提示信息 ====================");
+        log.warn("==================== WARN提示信息 ====================");
+
+        SpringApplication.run(EurekaServer_7003.class, args);
+    }
+
+}
